1fa147bf8c3f3df15ce828b1d19e9e05ce7ae3e3
WilliamWJD <william.jose.wjd@gmail.com>
2024-01-19 09:26:21 -0300

    feat: utilizando hook useMemo para calculo de transactions

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { TransactionsContext } from '../context/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
@@ -6,24 +7,26 @@ export function useSummary() {
     return context.transactions
   })
 
-  const summary = transactions.reduce(
-    (acm, transaction) => {
-      if (transaction.type === 'income') {
-        acm.income += transaction.price
-        acm.total += transaction.price
-      } else {
-        acm.outcome += transaction.price
-        acm.total -= transaction.price
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acm, transaction) => {
+        if (transaction.type === 'income') {
+          acm.income += transaction.price
+          acm.total += transaction.price
+        } else {
+          acm.outcome += transaction.price
+          acm.total -= transaction.price
+        }
 
-      return acm
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+        return acm
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
